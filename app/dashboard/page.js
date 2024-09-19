41c1c08acc71e4f37e6f2238f91c51a3225e44ba
Zohaib Alam Pervaiz <ikillar840@gmail.com>
2024-09-19 16:55:53 +0500

    dashboard style

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -11,6 +11,25 @@ import Link from 'next/link';
 export default function HomePage() {
   const [selectedYear, setSelectedYear] = useState('2024');
   const [userType, setUserType] = useState('Individual');
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [filePreview, setFilePreview] = useState(null); // Preview for images
+
+  // Function to handle file input change
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    setSelectedFile(file);
+
+    // If the file is an image, set the preview
+    if (file && file.type.startsWith('image/')) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setFilePreview(reader.result); // Set image preview data URL
+      };
+      reader.readAsDataURL(file);
+    } else {
+      setFilePreview(null); // Reset preview if it's not an image
+    }
+  };
   
   useEffect(() => {
     // Trigger progress bar animation on page load
@@ -29,9 +48,9 @@ export default function HomePage() {
 
   return (
     <div className="min-h-screen bg-gray-200 flex flex-col p-6 text-black ">
-      <div className='flex space-x-10 pl-10 mb-20 w-full'>
+      <div className='flex space-x-16 pl-10 mb-20 w-full'>
       <Image src={logo} className='w-10 h-10 '/>
-      <div className="flex justify-between mb-8">
+      <div className="flex justify-between mb-8 ">
         <div className="flex space-x-4 ">
             <select
               value={selectedYear}
@@ -125,24 +144,117 @@ export default function HomePage() {
           </div>
           <div className="flex flex-col items-center space-y-2">
             <div className="relative">
-             <Image src={more} className='w-10 h-10 '/>
+             <Image src={more} className='w-6 h-6 '/>
             </div>
           </div>
         </div>
 
         {/* File Upload Section */}
-        <div className="border border-dashed border-gray-300 p-8 rounded-lg text-center">
-          <div className="flex flex-col items-center space-y-2">
-            <svg className="w-16 h-16 text-gray-400 mb-4" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <div className="grid grid-cols-1 md:grid-cols-8 gap-8 mb-12 items-center">
+          <div>
+            <select
+                // value={selectedYear}
+                // onChange={(e) => setSelectedYear(e.target.value)}
+                className="border border-gray-300 rounded-md p-3 text-lg">
+                <option value="w2">w2</option>
+                <option value="1049">1049</option>
+                {/* Add more years as needed */}
+              </select>
+          </div>
+          <div className="flex flex-col items-center space-y-2 justify-center content-center">
+          <div className="flex flex-col items-center bg-white justify-center p-4 content-center">
+            <svg
+              className="w-10 h-10 text-gray-400 mb-4"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg">
               <path strokeLinecap="round" strokeLinejoin="round" d="M7 16v-4a4 4 0 014-4h6"></path>
               <path strokeLinecap="round" strokeLinejoin="round" d="M3 12l9 9 9-9"></path>
             </svg>
-            <p className="text-lg">Click or drag a file to this area to upload.</p>
-            <input type="file" className="hidden" />
-            <button className="bg-blue-500 text-white px-6 py-2 rounded-md mt-4">Upload</button>
+
+            {/* Display either the file name or image preview */}
+            {selectedFile ? (
+              <div className="w-full flex flex-col items-center space-y-2">
+                {filePreview ? (
+                  <img
+                    src={filePreview}
+                    alt="Preview"
+                    className="w-32 h-32 object-contain mb-2"
+                  />
+                ) : (
+                  <p className="text-sm text-gray-700">{selectedFile.name}</p>
+                )}
+              </div>
+            ) : (
+              <p className="text-1xl">Click or drag a file to this area to upload.</p>
+            )}
+
+            {/* Hidden input field for file selection */}
+            <input
+              type="file"
+              className="hidden"
+              id="file-upload"
+              onChange={handleFileChange}
+            />
+            
+            {/* Triggering the hidden file input */}
+            
           </div>
+          <button
+              className="bg-blue-500 text-white px-6 py-2 rounded-md mt-4"
+              onClick={() => document.getElementById('file-upload').click()}
+            >
+              Upload
+            </button>
           <p className="text-sm text-gray-500 mt-2">Supports: PDF, JPEG, PNG, TIFF, Excel, CSV</p>
         </div>
+          <div>
+            {selectedFile ? <p className="font-semibold p-5 text-gray-500 text-center">{selectedFile.name}</p> : <p className="font-semibold p-5 text-gray-500 text-center text-2xl">...</p> }  
+          </div>
+          <div>
+              <select
+                // value={selectedYear}
+                // onChange={(e) => setSelectedYear(e.target.value)}
+                className="border border-gray-300 rounded-md p-3 text-lg">
+                <option value="self">self</option>
+                <option value="one a year">one a year</option>
+                {/* Add more years as needed */}
+              </select>
+          </div>
+          <div className="flex flex-col items-center space-y-2">
+            <div className="relative">
+              <svg className="w-12 h-12 text-blue-500" viewBox="0 0 100 100">
+                <circle cx="50" cy="50" r="45" stroke="currentColor" strokeWidth="10" fill="none" className="opacity-30" />
+                <circle cx="50" cy="50" r="45" stroke="currentColor" strokeWidth="10" fill="none" strokeDasharray="283" strokeDashoffset="283" className="progress-circle" />
+              </svg>
+            </div>
+          </div>
+
+          <div className="flex flex-col items-center space-y-2">
+            <div className="relative">
+              <svg className="w-12 h-12 text-blue-500" viewBox="0 0 100 100">
+                <circle cx="50" cy="50" r="45" stroke="currentColor" strokeWidth="10" fill="none" className="opacity-30" />
+                <circle cx="50" cy="50" r="45" stroke="currentColor" strokeWidth="10" fill="none" strokeDasharray="283" strokeDashoffset="283" className="progress-circle" />
+              </svg>
+            </div>
+          </div>
+          <div className="flex flex-col items-center space-y-2">
+            <div className="relative">
+              <svg className="w-12 h-12 text-blue-500" viewBox="0 0 100 100">
+                <circle cx="50" cy="50" r="45" stroke="currentColor" strokeWidth="10" fill="none" className="opacity-30" />
+                <circle cx="50" cy="50" r="45" stroke="currentColor" strokeWidth="10" fill="none" strokeDasharray="283" strokeDashoffset="283" className="progress-circle" />
+              </svg>
+            </div>
+            </div>
+          <div className="flex flex-col items-center space-y-2">
+            <div className="relative">
+             <Image src={more} className='w-6 h-6 '/>
+            </div>
+          </div>
+        </div>
+
       </div>
     </div>
   );
